3075b7818f5ea600e750fff57b5b9e46d6f5fe0d
Fernando Souza <nandosouzafilho@gmail.com>
2020-10-11 16:52:05 +0200

    Defining global styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 40%;
+
+    @media screen and (min-width: 768px) {
+      font-size: 30%;
+    }
+
+    @media screen and (min-width: 992px) {
+      font-size: 40%;
+    }
+
+    @media screen and (min-width: 1400px) {
+      font-size: 62.5%;
+    }
+  }
+
+  button {
+    outline: none;
+  }
+
+  body {
+    margin: 0;
+    font-family: "Roboto";
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    background-color: #262a59;
+  }
+
+  html, body, #root {
+    min-height: 100vh;
+  }
+
+  #root {
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
